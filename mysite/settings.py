a3978de5c8f37fd9f6469eed602b71c8d32c50cc
volleyballer5 <mpeavy2018@gmail.com>
2017-07-07 15:03:56 -0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'gw%m0qg9f#*h&jwle$ffc!54lt6q-5n%n&%&pm3l9f7w1q7jl+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'volleyballer5.pythonanywhere.com']
 
